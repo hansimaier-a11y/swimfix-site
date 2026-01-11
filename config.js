18d9b496b8a9b130ab7ab68dadf06fcc30bd858f
hansimaier-a11y <lkorber.perner@gmail.com>
2026-01-11 21:21:47 +0100

    Add Stripe payment links

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ window.SWIMFIX = {
   brand: "SwimFix",
   supportEmail: "info.coldcharge@gmail.com",
   stripe: {
-    quick: "YOUR_STRIPE_LINK_QUICK",
-    full: "YOUR_STRIPE_LINK_FULL",
-    monthly: "YOUR_STRIPE_LINK_MONTHLY"
+    quick: "https://buy.stripe.com/14AfZifydbR71Kvbbuao800",
+    full: "https://buy.stripe.com/00wcN699P08p0Grcfyao801",
+    monthly: "https://buy.stripe.com/eVqcN63Pv08pbl55Raao802"
   },
   currencySymbol: "€",
   deliveryHours: "24–48",
